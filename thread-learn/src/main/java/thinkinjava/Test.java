bf839e41f387d1f04d50cca5b394b34021390813
wangjin <wkingj@outlook.com>
2019-05-08 21:24:26 +0800

    线程学习

--- thread-learn/src/main/java/thinkinjava/Test.java
+++ thread-learn/src/main/java/thinkinjava/Test.java
@@ -2,6 +2,7 @@ package thinkinjava;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class Test implements Runnable {
 
@@ -25,14 +26,28 @@ public class Test implements Runnable {
 	}
 
 	public static void main(String[] args) {
-		ExecutorService exec = Executors.newCachedThreadPool();
-		Test test = new Test();
+		//compute(19_000);
+		Float dt = 0.2f;
 
-		Boolean err = null;
-
+		System.out.println();
 	}
 
 
+	static int compute(int n) {
+		if (n == 1)
+			return 1;
+		else {
+			return n * compute(n - 1);
+		}
+	}
+
+	static int compute(int n, int result) {
+		if (n == 1)
+			return result;
+		else {
+			return compute(n - 1, n * result);
+		}
+	}
 
 
 }
